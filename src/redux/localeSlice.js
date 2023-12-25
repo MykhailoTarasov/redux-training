f7e8384b00e15c53a879be38fa80926d1eb9a59b
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-12-25 10:52:58 +0200

    the lesson is done

--- src/redux/localeSlice.js
+++ src/redux/localeSlice.js
@@ -1,22 +1,20 @@
-const initialState = {
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const localeSlice = createSlice({
+  name: 'locale',
+  initialState: {
     lang: 'en',
-  };
-
-  export const localeReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'locale/changeLang':
-        return {
-          ...state,
-          lang: action.payload,
-        };
-      default:
-        return state;
+  },
+  reducers: {
+    changeLang(state, action) {
+      state.lang = action.payload
     }
-  };
+  }
+});
 
-  export const changeLang = newLang => {
-    return {
-      type: 'locale/changeLang',
-      payload: newLang,
-    };
-  };
+export const localeReducer = localeSlice.reducer;
+export const {changeLang} = localeSlice.actions;
+
+
+
